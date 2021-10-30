b2a8b51952d905a9a20a0688dbe24df01f214cd5
Daniel Zhao <xulinzh@yahoo.ca>
2021-10-30 00:31:47 -0400

    add solutions

--- src/main/java/com/dz/algorithms/solutions/Solution.java
+++ src/main/java/com/dz/algorithms/solutions/Solution.java
@@ -1,8 +1,16 @@
 package com.dz.algorithms.solutions;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
+import static com.dz.algorithms.util.Utilities.swap;
+
 public class Solution {
+    private Solution() {
+        //util
+    }
+
     static int sockMerchant(int n, List<Integer> ar) {
         int num = 0;
 
@@ -51,7 +59,290 @@ public class Solution {
         return count;
     }
 
-    private Solution(){
-        //util
+    static int jumpingOnClouds(List<Integer> c) {
+        int n = c.size();
+        int currentCloud = 0;
+        int num = 0;
+
+        while (currentCloud < n - 2) {
+            if (c.get(currentCloud + 2) == 0) {
+                currentCloud += 2;
+            } else {
+                currentCloud += 1;
+            }
+
+            num++;
+        }
+
+
+        if (currentCloud == n - 1) {
+            return num;
+        }
+
+        return num + 1;
+    }
+
+    static long repeatedString(String s, long n) {
+        if (s == null || s.isEmpty()) {
+            return 0;
+        }
+        if (s.equals("a")) {
+            return n;
+        }
+
+        int l = s.length();
+        int m = countA(s);
+
+        if (m == 0) {
+            return 0;
+        }
+
+        long times = n / l;
+        int remain = (int) (n % l);
+
+        long count = m * times;
+
+        if (remain == 0) {
+            return count;
+        }
+
+        return count + countA(s.substring(0, remain));
+    }
+
+    static int countA(String s) {
+        int l = s.length();
+        int m = 0;
+        for (int i = 0; i < l; ++i) {
+            if (s.charAt(i) == 'a') {
+                m++;
+            }
+        }
+
+        return m;
+    }
+
+    static List<Integer> rotLeft(List<Integer> a, int d) {
+        // Write your code here
+        List<Integer> shifted = new ArrayList<>();
+
+        for (int i = d; i < a.size(); ++i) {
+            shifted.add(a.get(i));
+        }
+
+        for (int i = 0; i < d; ++i) {
+            shifted.add(a.get(i));
+        }
+
+        return shifted;
+    }
+
+    static int minimumBribes(List<Integer> q) {
+        int count = 0;
+
+        int n = q.size();
+
+        for (int i = 0; i < n; ++i) {
+            if (q.get(i) - (i + 1) > 2) {
+                return -1;
+            }
+
+            for (int j = larger(q.get(i) - 2, 0); j < i; j++) {
+                if (q.get(j) > q.get(i)) {
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+
+    static int larger(int i, int j) {
+        return i > j ? i : j;
+    }
+
+    static int minimumSwaps(int[] arr) {
+        int n = arr.length;
+        if (n <= 1) {
+            return 0;
+        }
+
+        int count = 0;
+
+        int[] indexDict = new int[n + 1];
+
+        indexDict[0] = -1;//never used
+
+        for (int i = 0; i < n; ++i) {
+            indexDict[arr[i]] = i;
+        }
+
+        for (int i = 0; i < n; ++i) {
+            if (arr[i] != i + 1) {
+                int v = arr[i];
+                int p = indexDict[i + 1];
+                swap(arr, i, p);
+                count++;
+                indexDict[i + 1] = i;
+                indexDict[v] = p;
+            }
+        }
+
+        return count;
+    }
+
+    static String twoStrings(String s1, String s2) {
+        int[] charDict = new int[26];
+
+        String shortStr = s1;
+        String longStr = s2;
+
+        if (s1.length() > s2.length()) {
+            shortStr = s2;
+            longStr = s1;
+        }
+
+        for (int i = 0; i < shortStr.length(); ++i) {
+            int index = shortStr.charAt(i) - 'a';
+
+            charDict[index] = 1;
+        }
+
+        for (int i = 0; i < longStr.length(); ++i) {
+            int index = longStr.charAt(i) - 'a';
+
+            if (charDict[index] == 1) {
+                return "YES";
+            }
+        }
+
+        return "NO";
+    }
+
+    static int sherlockAndAnagrams(String s) {
+        int n = s.length();
+        String[][] dict = new String[n][n];
+
+        for (int i = 0; i < n; ++i) {
+            for (int j = i; j < n; ++j) {
+                dict[i][j] = sortChars(s, i, j);
+            }
+        }
+
+        HashMap<String, Integer> countMap = new HashMap<>();
+
+        for (int i = 0; i < n; ++i) {
+            for (int j = i; j < n; ++j) {
+                String key = dict[i][j];
+                int count = 1;
+                if (countMap.containsKey(key)) {
+                    count = countMap.get(key) + 1;
+                }
+
+                countMap.put(key, count);
+            }
+        }
+
+        int count = 0;
+
+        for (int v : countMap.values()) {
+            if (v > 1) {
+                count += (v * (v - 1)) / 2;
+            }
+        }
+
+        return count;
+    }
+
+    static String sortChars(String s, int begin, int end) {
+        int letterNum = 26;
+        int[] dict = new int[letterNum];
+
+        for (int i = begin; i <= end; ++i) {
+            int index = s.charAt(i) - 'a';
+            dict[index]++;
+        }
+
+        StringBuilder builder = new StringBuilder();
+
+        for (int i = 0; i < letterNum; ++i) {
+            while (dict[i] > 0) {
+                builder.append((char) (i + 'a'));
+                dict[i]--;
+            }
+        }
+
+        return builder.toString();
+    }
+
+    static long countTriplets(List<Long> arr, long r) {
+        if (r == 1L) {
+            return countTriplets(arr);
+        }
+
+        long count = 0;
+        HashMap<Long, Long> numberCountDict = new HashMap<>();
+        HashMap<String, Long> pairCountDict = new HashMap<>();
+
+        for (int i = 0; i < arr.size(); ++i) {
+            Long v = arr.get(i);
+            long n = 1;
+
+            if (numberCountDict.containsKey(v)) {
+                n = numberCountDict.get(v) + 1;
+            }
+
+            numberCountDict.put(v, n);
+
+            long p = v / r;
+            long pRemain = v % r;
+            long pp = v / (r*r);
+            long ppRemain = v % (r*r);
+
+            if (pRemain==0 && numberCountDict.containsKey(p)){
+                String pair = String.format("%d %d", v, p);
+
+                long m = numberCountDict.get(p);
+                if (pairCountDict.containsKey(pair)){
+                    m += pairCountDict.get(pair);
+                }
+
+                pairCountDict.put(pair, m);
+            }
+
+            if (ppRemain==0){
+                String pair = String.format("%d %d", p, pp);
+
+                if (pairCountDict.containsKey(pair)){
+                    count += pairCountDict.get(pair);
+                }
+            }
+        }
+
+        return count;
+    }
+
+    static long countTriplets(List<Long> arr) {
+        HashMap<Long, Long> dict = new HashMap<>();
+
+        for (int i = 0; i < arr.size(); ++i) {
+            Long v = arr.get(i);
+            long n = 1;
+
+            if (dict.containsKey(v)) {
+                n = dict.get(v) + 1;
+            }
+
+            dict.put(v, n);
+        }
+
+        long count = 0;
+
+        for (long num : dict.values()) {
+            if (num >= 3) {
+                count += num * (num - 1) * (num - 2) / 6;
+            }
+        }
+
+        return count;
     }
 }
--- src/test/java/com/dz/algorithms/solutions/SolutionTest.java
+++ src/test/java/com/dz/algorithms/solutions/SolutionTest.java
@@ -2,9 +2,17 @@ package com.dz.algorithms.solutions;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static com.dz.algorithms.solutions.Solution.*;
+import static com.dz.algorithms.solutions.Solution.countTriplets;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class SolutionTest {
@@ -17,5 +25,73 @@ class SolutionTest {
     @Test
     void testCountingValleys(){
         assertEquals(1, countingValleys(8, "UDDDUDUU"));
+    }
+
+    @Test
+    void testJumpingOnClouds(){
+        assertEquals(4, jumpingOnClouds(Arrays.asList(0, 0, 1, 0, 0, 1, 0)));
+    }
+
+    @Test
+    void testRepeatedString(){
+        assertEquals(7, repeatedString("aba", 10L));
+    }
+
+    @Test
+    void testRotLeft(){
+        assertArrayEquals(new Integer[]{5, 1, 2, 3, 4}, rotLeft(Arrays.asList(1, 2, 3, 4, 5), 4).toArray(new Integer[0]));
+    }
+
+    @Test
+    void testMinimumBribes(){
+        assertEquals(3, minimumBribes(Arrays.asList(2, 1, 5, 3, 4)));
+        assertEquals(-1, minimumBribes(Arrays.asList(2, 5, 1, 3, 4)));
+        assertEquals(7, minimumBribes(Arrays.asList(1, 2, 5, 3, 7, 8, 6, 4)));
+    }
+
+    @Test
+    void testMinimumSwaps(){
+        assertEquals(5, minimumSwaps(new int[]{7, 1, 3, 2, 4, 5, 6}));
+        assertEquals(3, minimumSwaps(new int[]{4, 3, 1, 2}));
+        assertEquals(3, minimumSwaps(new int[]{2, 3, 4, 1, 5}));
+        assertEquals(3, minimumSwaps(new int[]{1, 3, 5, 2, 4, 6, 7}));
+    }
+
+    @Test
+    void testTwoStrings(){
+        String yes = "YES";
+        String no = "NO";
+        assertEquals(yes, twoStrings("hello", "world"));
+        assertEquals(yes, twoStrings("kid", "world"));
+        assertEquals(no, twoStrings("hi", "world"));
+    }
+
+    @Test
+    void testSherlockAndAnagrams(){
+        assertEquals(2, sherlockAndAnagrams("mom"));
+        assertEquals(10, sherlockAndAnagrams("kkkk"));
+        assertEquals(3, sherlockAndAnagrams("ifailuhkqq"));
+    }
+
+    @Test
+    void testCountTriplets() throws URISyntaxException, IOException {
+        assertEquals(2, countTriplets(Arrays.asList(1L, 2L, 2L, 4L), 2L));
+        assertEquals(6, countTriplets(Arrays.asList(1L, 3L, 9L, 9L, 27L, 81L), 3L));
+        assertEquals(4, countTriplets(Arrays.asList(1L, 5L, 5L, 25L, 125L), 5L));
+
+        assertEquals(161700, countTriplets(Arrays.asList(1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L,
+                1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L,
+                1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L,
+                1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L), 1L));
+
+        assertEquals(161700, countTriplets(Arrays.asList(5L, 5L, 5L, 5L, 5L, 5L, 5L, 5L, 5L, 5L, 5L, 5L, 5L, 5L, 5L, 5L, 5L,
+                5L, 5L, 5L, 5L, 5L, 5L, 5L, 5L, 5L, 5L, 5L, 5L, 5L, 5L, 5L, 5L, 5L, 5L, 5L, 5L, 5L, 5L, 5L, 5L, 5L, 5L, 5L, 5L, 5L,
+                5L, 5L, 5L, 5L, 5L, 5L, 5L, 5L, 5L, 5L, 5L, 5L, 5L, 5L, 5L, 5L, 5L, 5L, 5L, 5L, 5L, 5L, 5L, 5L, 5L, 5L, 5L, 5L, 5L,
+                5L, 5L, 5L, 5L, 5L, 5L, 5L, 5L, 5L, 5L, 5L, 5L, 5L, 5L, 5L, 5L, 5L, 5L, 5L, 5L, 5L, 5L, 5L, 5L, 5L), 1L));
+
+        String numbers = Files.readString( Path.of(SolutionTest.class.getClassLoader().getResource("numbers.txt").toURI()));
+        List<Long> numberList =  Arrays.stream(numbers.trim().split(" ")).map(Long::parseLong).collect(Collectors.toList());
+
+        assertEquals(2325652489L, countTriplets(numberList, 3L));
     }
 }
